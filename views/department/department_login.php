57a1ab2bf313b203437a1d383216450aa373081c
shubhammachal <shubham95m@gmail.com>
2017-06-17 11:06:58 +0530

    login interface modified

--- views/department/department_login.php
+++ views/department/department_login.php
@@ -1,6 +1,8 @@
 <?php
       include_once('views/includes/includes_header.php');
 ?>
+
+
 <nav class="mdl-navigation">
       <a class="mdl-navigation__link" href="/about">About</a>
       <a class="mdl-navigation__link" href="/contact">Contact</a>
@@ -16,23 +18,16 @@
     <div class="mdl-grid">
 
 
- <form>
- <h2 class="dept">Department Login</h2>
-  <div class="group">
-    <input type="text"><span class="highlight" name="uname" pattern="[A-Za-z0-9]{1,15}" placeholder="Letters & Numerics" id="uname" required></span><span class="bar"></span>
-   
-    <label>User Name</label>
-  </div>
-  <div class="group">
-    <input type="password"><span class="highlight" type="password" name="password"  pattern="[A-Za-z0-9]+" id="pass" required></span><span class="bar"></span>
-    <label>Password</label>
-  </div>
 
-  <a href="#" target="_blank">Forgot Password</a>
 
-<button type="button" class="button buttonBlue">Login
-    <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
-  </button>
+  <form>
+<h1 class="dept">Department login</h1>
+<input placeholder="Username" pattern="[A-Za-z0-9]{1,15}" type="text" required="">
+<input placeholder="Password" pattern="[A-Za-z0-9]+" type="password" required="">
+
+<button class="login">Login</button>
+<a href="#" target="_blank">Forgot Password</a>
+
+
+
 </form>
-  
-
