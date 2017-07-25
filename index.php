e9a2c639857a18320cb25901c89f172b79a685e3
Rishabh04-02 <rishabh0402@gmail.com>
2017-07-25 10:36:02 +0530

    routing updated
    new routing added, routes created, .htaccess added

--- index.php
+++ index.php
@@ -1,200 +1,39 @@
 <?php
-// Grabs the URI and breaks it in parts
-$request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
 
-// URL Routing 
-switch ($request_uri[0]) {
-
-    //home, root    
-    case '/':
-        include_once('views/public/public_index.php');
-        break;
-    
-    //about page
-    case '/about':
-        include_once('views/public/public_about.php');
-        break;
-    
-    //contact page
-    case '/contact':
-        include_once('views/public/public_contact.php');
-        break;
-
-    //bugs reporting page
-    case '/bugs':
-        include_once('views/public/public_bugs.php');
-        break;
-
-    //credits page
-    case '/credits':
-        include_once('views/public/public_credits.php');
-        break;
-
-    //account activation page
-    case '/activate':
-        include_once('views/public/public_account_activate.php');
-        break;
-
-    //frequently asked questions
-    case '/faq':
-        include_once('views/public/public_faq.php');
-        break;
-
-     //Interfaces   
-
-    // Admin interface
-        //home page
-    case '/admin':
-        include_once('views/admin/admin_index.php');
-        break;
-
-        //login page
-    case '/admin/login':
-        include_once('views/admin/admin_login.php');
-        break;
-
-        //registration page
-    case '/admin/register':
-        include_once('views/admin/admin_register.php');
-        break;
-
-        //logout page
-    case '/admin/logout':
-        include_once('views/public/public_logout.php');
-        break;
-
-        //admin profile
-    case '/admin/profile':
-        include_once('views/admin/admin_profile.php');
-        break;
-
-        //admin password recovery
-    case '/admin/forget':
-        include_once('views/admin/admin_forget.php');
-        break;
-
-        //admin view registered students
-    case '/admin/profile/registered':
-        include_once('views/admin/admin_registered.php');
-        break;
-
-        //admin password change
-    case '/admin/change':
-        include_once('views/admin/admin_change.php');
-        break;
-
-
-    // Student interface
-        //student home page
-    case '/student':
-        include_once('views/student/student_index.php');
-        break;
-
-        //student list 
-    case '/student/list':
-        include_once('views/student/student_list.php');
-        break;
-
-        //student unregistered account deletion
-    case '/student/delete':
-        include_once('views/student/student_delete.php');
-        break;
-
-        //student login
-    case '/student/login':
-        include_once('views/student/student_login.php');
-        break;
-
-        //student registration
-    case '/student/register':
-        include_once('views/student/student_register.php');
-        break;
-
-        //student profile
-    case '/student/profile':
-        include_once('views/student/student_profile.php');
-        break;
-
-        //student password change
-    case '/student/change':
-        include_once('views/student/student_change.php');
-        break;
-
-        //student prioritize electives
-    case '/student/profile/apply':
-        include_once('views/student/student_apply.php');
-        break;
-
-        //Student Forget
-    case '/student/forget':
-        include_once('views/student/student_forget.php');
-        break;
-
-        //student applied
-    case '/student/profile/applied':
-        include_once('views/student/student_applied.php');
-        break;
-
-        //student logout
-    case '/student/logout':
-        include_once('views/public/public_logout.php');
-        break;
-
-
-    // Department interface
-        //department home page
-    case '/department':
-        include_once('views/department/department_index.php');
-        break;
-
-        //department login
-    case '/department/login':
-        include_once('views/department/department_login.php');
-        break;
-
-        //department registration
-    case '/department/register':
-        include_once('views/department/department_register.php');
-        break;
-
-        //department superuser profile
-    case '/department/profile':
-        include_once('views/department/department_profile.php');
-        break;
-
-        //department normaluser registration
-    case '/department/profile/register':
-        include_once('views/department/department_register.php');
-        break;
-
-    //department elective publish
-    case '/department/profile/publish':
-        include_once('views/department/department_publish.php');
-        break;
-
-        //department password change
-    case '/department/change':
-        include_once('views/department/department_change.php');
-        break;
-
-        //department password recovery
-    case '/department/forget':
-        include_once('views/department/department_forget.php');
-        break;
-
-        //department logout
-    case '/department/logout':
-        include_once('views/public/public_logout.php');
-        break;
-
-    // Everything else - The default 404 page
-    default:
-        header('HTTP/1.0 404 Not Found');
-        include_once('views/public/public_404.php');
-        break;
+function request_path()
+{
+    $request_uri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+    $script_name = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
+    $parts = array_diff_assoc($request_uri, $script_name);
+    if (empty($parts))
+    {
+        return '/';
+    }
+    $path = implode('/', $parts);
+    if (($position = strpos($path, '?')) !== FALSE)
+    {
+        $path = substr($path, 0, $position);
+    }
+    return $path;
 }
 
-    //footer here
-    include_once('views/includes/includes_footer.php');
+//including the routes file
+include_once('routes.php');
+
+$path = request_path();
+
+if (isset($routes[$path]) AND is_callable($routes[$path]))
+{
+    $routes[$path]();
+}
+else
+{
+    // Everything else - The default 404 page
+    header('HTTP/1.0 404 Not Found');
+    include_once('views/public/public_404.php');
+}
+
+//footer here
+include_once('views/includes/includes_footer.php');
 
 ?>
---
+++ routes.php
@@ -0,0 +1,150 @@
+<?php
+
+$routes = [
+    '' => function()    {
+        //home, root    
+        include_once('views/public/public_index.php');
+    },
+    'about' => function()   {
+        //about page
+        include_once('views/public/public_about.php');
+    },
+    'contact' => function()    {
+        //contact page
+        include_once('views/public/public_contact.php');
+    },
+    'credits' => function()    {
+        //credits page
+        include_once('views/public/public_credits.php');
+    },
+    'activate' => function()    {
+        //account activation page
+        include_once('views/public/public_account_activate.php');
+    },
+    'faq' => function()    {
+        //frequently asked questions
+        include_once('views/public/public_faq.php');
+    },
+    //Interfaces   
+
+    // Admin interface
+    'admin' => function()    {
+        //home page  
+        include_once('views/admin/admin_index.php');
+    },
+    'admin/login' => function()    {
+        //login page
+        include_once('views/admin/admin_login.php');
+    },
+    'admin/register' => function()    {
+        //registration page
+        include_once('views/admin/admin_register.php');
+    },
+    'admin/logout' => function()    {
+        //logout page
+        include_once('views/public/public_logout.php');
+    },
+    'admin/profile' => function()    {
+        //admin profile  
+        include_once('views/admin/admin_profile.php');
+    },
+    'admin/forget' => function()    {
+        //admin password recovery
+        include_once('views/admin/admin_forget.php');
+    },
+    'admin/profile/registered' => function()    {
+        //admin view registered students
+        include_once('views/admin/admin_registered.php');
+    },
+    'admin/change' => function()    {
+        //admin password change
+        include_once('views/admin/admin_change.php');
+    },
+    // Student interface
+
+    'student' => function()    {
+        //student home page
+        include_once('views/student/student_index.php');
+    },
+    'student/list' => function()    {
+        //student list    
+        include_once('views/student/student_list.php');
+    },
+    'student/delete' => function()    {
+        //student unregistered account deletion
+        include_once('views/student/student_delete.php');
+    },
+    'student/login' => function()    {
+        //student login    
+        include_once('views/student/student_login.php');
+    },
+    'student/register' => function()    {
+        //student registration  
+        include_once('views/student/student_register.php');
+    },
+    'student/profile' => function()    {
+        //student profile
+        include_once('views/student/student_profile.php');
+    },
+    'student/change' => function()    {
+        //student password change
+        include_once('views/student/student_change.php');
+    },
+    'student/profile/apply' => function()    {
+        //student prioritize electives
+        include_once('views/student/student_apply.php');
+    },
+    'student/forget' => function()    {
+        //Student Forget
+        include_once('views/student/student_forget.php');
+    },
+    'student/profile/applied' => function()    {
+        //student applied
+        include_once('views/student/student_applied.php');
+    },
+    'student/logout' => function()    {
+        //student logout
+        include_once('views/public/public_logout.php');
+    },
+    // Department interface
+
+    'department' => function()    {
+        //department home page  
+        include_once('views/department/department_index.php');
+    },
+    'department/login' => function()    {
+        //department login
+        include_once('views/department/department_login.php');
+    },
+    'department/register' => function()    {
+        //department registration 
+        include_once('views/department/department_register.php');
+    },
+    'department/profile' => function()    {
+        //department users profile
+        include_once('views/department/department_profile.php');
+    },
+    'department/profile/register' => function()    {
+        //department normaluser registration
+        include_once('views/department/department_register.php');
+    },
+    'department/profile/publish' => function()    {
+        //department elective publish
+        include_once('views/department/department_publish.php');
+    },
+    'department/change' => function()    {
+        //department password change
+        include_once('views/department/department_change.php');
+    },
+    'department/forget' => function()    {
+        //department password recovery
+        include_once('views/department/department_forget.php');
+    },
+    'department/logout' => function()    {
+        //department logout
+        include_once('views/public/public_logout.php');
+    }
+];
+
+
+?>
