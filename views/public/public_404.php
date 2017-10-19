1ec3654a9d78cffc443ce36061cc60749a40fb2e
Sam <thomas.sam64@gmail.com>
2017-10-19 09:29:59 +0530

    404 page (#24)

--- views/public/public_404.php
+++ views/public/public_404.php
@@ -14,30 +14,24 @@
     <div class="page-content">
     <!-- Your content goes here -->
 	 <center>
-  		<div class="demo-card-wide1 mdl-card mdl-shadow--4dp">
-			  <div class="mdl-card__title">
-			    <h2 class="mdl-card__title-text"><b>404 - Elective Manager</b></h2>
-			  </div>
-			  <div class="mdl-card__actions mdl-card--border"></div>
-			  <p style="text-align: left;">
-			  	The project prototype was developed by<a href="http://exe.nith.ac.in/" style="text-decoration: none;"><b> Team .EXE</b></a> during NIMBUS 2017, Under the guidance of <a href="#" style="text-decoration: none;"><b> Dr. Naveen Chauhan</b></a> (Faculty coordinator, Team .EXE) & the coordination of <a href="https://github.com/Rishabh04-02" style="text-decoration: none;"> Rishabh Chaudhary</a> & <a href="https://github.com/MayankKashyap" style="text-decoration: none;"> Mayank Kashyap</a>.<br>
-			  	This project was developed for implementation under the guidance of <a href="#" style="text-decoration: none;"><b>Dr. Naveen Chauhan</b></a> and was developed by the following developers.
-			  </p>
-			  <div class="mdl-card__actions mdl-card--border"></div>
-			  <div style="text-align: left;">
-			  <b>Developers -</b>
-			  <ul>
-			  <li><a style="text-decoration: none;" href="https://github.com/Rishabh04-02" target="_blank">14MI508 - <b>Rishabh Chaudhary</b></a></li>
-			  <li><a style="text-decoration: none;" href="https://github.com/Abhishek-sopho" target="_blank">14MI510 - <b>Abhishek Kumar</b></a></li>
-			  <li><a style="text-decoration: none;" href="https://github.com/Shubhammachal" target="_blank">14MI541 - <b>Shubham Machal</b></a></li>
-			  </ul>
-			  </div>
-			  <div class="mdl-card__actions mdl-card--border"></div>
-			  <div style="text-align: left;">
-			  More information available here - <a href="https://github.com/Rishabh04-02/Elective-manager/graphs/contributors" style="text-decoration: none;">GitHub : Elective-manager</a>
-			  </div>
+		<div class="demo-card-square mdl-card mdl-shadow--2dp" style="margin-top: 25px;width: 90%;">
+			<div class="sadface-wrapper" style="margin: 40px 0 10px 0;">
+				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 49 49" style="enable-background:new 0 0 49 49;" xml:space="preserve" width="100px" height="100px">
+					<g>
+						<path d="M24.5,29c-6.617,0-12,5.383-12,12c0,0.552,0.448,1,1,1s1-0.448,1-1c0-5.514,4.486-10,10-10s10,4.486,10,10   c0,0.552,0.448,1,1,1s1-0.448,1-1C36.5,34.383,31.117,29,24.5,29z" fill="#9b9b9b"/>
+						<path d="M15.5,20c1.654,0,3,1.346,3,3c0,0.552,0.448,1,1,1s1-0.448,1-1c0-2.757-2.243-5-5-5s-5,2.243-5,5c0,0.552,0.448,1,1,1   s1-0.448,1-1C12.5,21.346,13.846,20,15.5,20z" fill="#9b9b9b"/>
+						<path d="M33.5,18c-2.757,0-5,2.243-5,5c0,0.552,0.448,1,1,1s1-0.448,1-1c0-1.654,1.346-3,3-3s3,1.346,3,3c0,0.552,0.448,1,1,1   s1-0.448,1-1C38.5,20.243,36.257,18,33.5,18z" fill="#9b9b9b"/>
+						<path d="M24.5,5c-1.711,0-3.435,0.221-5.14,0.634C18.349,2.775,16.695,0,14.5,0c-3.806,0-6,8.348-6,11   c0,0.292,0.028,0.577,0.068,0.857C4.661,15.964,2.5,21.311,2.5,27c0,12.131,9.869,22,22,22s22-9.869,22-22S36.631,5,24.5,5z    M14.5,2c1.101,0,2.432,2.538,3.247,5.104c0.002,0.007,0,0.015,0.002,0.023C18.212,8.612,18.5,10.096,18.5,11c0,2.206-1.794,4-4,4   c-0.256,0-0.505-0.029-0.748-0.076c-0.082-0.016-0.159-0.042-0.239-0.062c-0.157-0.04-0.312-0.085-0.461-0.143   c-0.091-0.036-0.179-0.078-0.266-0.12c-0.128-0.061-0.251-0.128-0.371-0.202c-0.091-0.056-0.181-0.115-0.267-0.178   c-0.099-0.073-0.192-0.153-0.284-0.235c-0.092-0.082-0.185-0.162-0.269-0.252c-0.059-0.063-0.11-0.133-0.165-0.2   c-0.375-0.459-0.662-1.001-0.809-1.612c-0.004-0.018-0.014-0.033-0.019-0.05C10.539,11.589,10.5,11.3,10.5,11   C10.5,8.175,12.782,2,14.5,2z M24.5,47c-11.028,0-20-8.972-20-20c0-4.824,1.696-9.387,4.807-13.008   c0.129,0.223,0.273,0.437,0.429,0.641c0.005,0.006,0.009,0.013,0.014,0.019c0.301,0.391,0.647,0.743,1.033,1.05   c0.049,0.039,0.1,0.075,0.15,0.112c0.139,0.103,0.283,0.201,0.431,0.292c0.061,0.038,0.122,0.076,0.184,0.112   c0.195,0.111,0.395,0.214,0.603,0.303c0.02,0.009,0.041,0.016,0.061,0.024c0.201,0.083,0.408,0.153,0.618,0.214   c0.071,0.021,0.143,0.037,0.215,0.055c0.167,0.042,0.337,0.076,0.51,0.104c0.072,0.011,0.143,0.024,0.215,0.033   C14.01,16.98,14.253,17,14.5,17c3.309,0,6-2.691,6-6c0-0.177-0.011-0.383-0.031-0.607c-0.015-0.192-0.04-0.397-0.069-0.608   c-0.001-0.009-0.002-0.018-0.003-0.027c-0.092-0.676-0.24-1.428-0.449-2.221C21.46,7.18,22.987,7,24.5,7c11.028,0,20,8.972,20,20   S35.528,47,24.5,47z" fill="#9b9b9b"/>
+					</g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
+				</svg>
+			</div>
+			
+			<div style="width: 230px; margin: 0 0 20px 0">
+				<h2 style="margin: 5px 0 5px 0;color: #9B9B9B;font-family: Arial, Helvetica, sans-serif;"><i>Whoops!</i></h2>
+				<p style="color: #9B9B9B;font-family: Arial, Helvetica, sans-serif; font-size: 1.2rem;">We couldn't find the page you were looking for.</p>
+			</div>
 		</div>
-		</center>
+	 </center>
 
   	
    </div>
